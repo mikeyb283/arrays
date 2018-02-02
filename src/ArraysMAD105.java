ae6d9710e78e28851a39acd550bef6ad6f8060d5
Michael Balling <mballing@students.mchenry.edu>
2018-02-02 16:57:05 -0600

    Initial commit

---
+++ src/ArraysMAD105.java
@@ -0,0 +1,12 @@
+public class ArraysMAD105 {
+
+   public static void main(String[] args){
+      String[] hotDogSupplies = {"Relish", "Mustard", "Pickles",
+        "Onions", "Sport Peppers", "Celery Salt", "Buns", "Hot Dogs"};
+
+      for (int i = 0; i < hotDogSupplies.length; i++) {
+        System.out.println(hotDogSupplies[i]);
+      }
+
+  }
+}
